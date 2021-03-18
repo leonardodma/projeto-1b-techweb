4fdc4b944a62ac76a1ebbfdbd65430c5ab588b46
leonardodma <leonardodma@al.insper.edu.br>
2021-03-18 09:18:05 -0300

    handout pronto

--- notes/models.py
+++ notes/models.py
@@ -4,3 +4,6 @@ from django.db import models
 class Note(models.Model):
     title = models.CharField(max_length=200)
     content = models.TextField(max_length=1000)
+
+    def __str__(self):
+        return f'{self.id}. {self.title}'
--- notes/views.py
+++ notes/views.py
@@ -1,7 +1,18 @@
 from django.shortcuts import render
 
 # Create your views here.
-from django.http import HttpResponse
- 
+from django.shortcuts import render, redirect
+from .models import Note
+
+
 def index(request):
-    return HttpResponse("Olá mundo! Este é o app notes de Tecnologias Web do Insper.")  
+    if request.method == 'POST':
+        title = request.POST.get('titulo')
+        content = request.POST.get('detalhes')
+        # TAREFA: Utilize o title e content para criar um novo Note no banco de dados
+
+        Note(title=title, content=content).save()
+        return redirect('index')
+    else:
+        all_notes = Note.objects.all()
+        return render(request, 'notes/index.html', {'notes': all_notes})
